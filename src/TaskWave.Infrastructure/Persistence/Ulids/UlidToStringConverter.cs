ad86d148eb4e4e1c9296573a350783d93c22ad17
Daniel <xklapal2@mendelu.cz>
2024-09-19 22:46:32 +0200

    Created: Ulid persistance converter, Entity and Group configuration; Modified: UserConfiguration - inherits from EntityConfiguration

---
+++ src/TaskWave.Infrastructure/Persistence/Ulids/UlidToStringConverter.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace TaskWave.Infrastructure.Persistence.Ulids;
+
+/// <summary>
+/// Ulid to String and vice-versa converter.
+/// </summary>
+public class UlidToStringConverter : ValueConverter<Ulid, string>
+{
+    public UlidToStringConverter()
+        : base(
+            ulid => ulid.ToString(), // Convert Ulid to string for storage
+            str => Ulid.Parse(str) // Convert string back to Ulid for usage
+        )
+    { }
+}
